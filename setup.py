708d8f78987294d833a91b3a949860e88823b60a
Your Name <you@example.com>
2022-10-15 20:32:04 +0000

    fix packaging

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name="metro_integrase",
-    version="0.0.2",
+    version="0.0.3",
     author="Rootspring",
     author_email="cheesycod@outlook.com",
     description="A Python wrapper for the Metro Reviews API",
     long_description="A Python wrapper for the Metro Reviews API (https://github.com/MetroReviews)",
-    include_dirs=["metro_integrase"],
+    packages=["metro_integrase"],
     install_requires=[
         "fastapi",
         "aiohttp",
